5f4a13097da2cf8b2d0c37b966a7c17d26854845
Yanny Esteban <yannyesteban@hotmail.com>
2019-07-31 12:38:25 -0400

    ok

---
+++ XTServer/XTConn.h
@@ -0,0 +1,26 @@
+#pragma once
+
+
+class XTDB {
+
+	static void listen(char* msg) {
+
+
+		switch (msg[0]) {
+		case '*':
+			break;
+		case '$':
+			break;
+		case '#':
+			break;
+
+
+		}
+
+
+	}
+
+
+
+
+};
